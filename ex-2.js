57cac3bf9f007a8ee872a1373cb2773435178471
noppadon <noppadonsangthongw@gmail.com>
2023-11-23 18:26:56 +0700

    edit ex2

--- ex-2.js
+++ ex-2.js
@@ -11,9 +11,9 @@ let isPlatinum;
 lastMonthPaidMoreThan4000 = true;
 isWeekday = true;
 hasBoughtProductFromITCategory = false;
-hasAttendedDiscountEvent = true;
+hasAttendedDiscountEvent = false;
 isPlatinum = false;
 
-let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory) || (isPlatinum);
+let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && hasAttendedDiscountEvent) || (isPlatinum);
 
 console.log(hasPromotion);
